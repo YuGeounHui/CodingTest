b856e00930fbac619b23c23536aee56b726716f8
유건희 <yukh0906@gmail.com>
2022-02-08 15:31:31 +0900

    BeakJoon 16194, 11052

--- src/SumOneTwoThree/Main.java
+++ src/SumOneTwoThree/Main.java
@@ -4,19 +4,22 @@ import java.util.*;
 
 public class Main {
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
+
+        Scanner scanner = new Scanner(System.in);
         int[] d = new int[11];
-        d[0] = 1;
-        for (int i=1; i<=10; i++) {
-            for (int j=1; j<=3; j++) {
-                if (i-j >= 0) {
+        d[0] = 1; // 0을 나타낼 수 있는건 1개
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 3; j++) {
+                if (i - j >= 0) {
                     d[i] += d[i-j];
                 }
             }
         }
-        int t = sc.nextInt();
+
+        int t = scanner.nextInt();
         while (t-- > 0) {
-            int n = sc.nextInt();
+            int n = scanner.nextInt();
             System.out.println(d[n]);
         }
     }
