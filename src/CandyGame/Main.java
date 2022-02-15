23f63d072e7ace56897b15ad5593e935628a3780
유건희 <20151309@deajin.ac.kr>
2022-02-15 21:52:16 +0900

    BeakJoon 3085

---
+++ src/CandyGame/Main.java
@@ -0,0 +1,15 @@
+package CandyGame;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        char[][] a = new char[n][n];
+
+    }
+}
